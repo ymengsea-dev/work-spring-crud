e94017270837686dec77ebceccf3957f2d656cb6
ymengsea <mrmengsea@gmail.com>
2026-03-09 20:05:14 +0700

    added response to oauth2 google

--- src/main/java/com/example/productcrud/config/OAuth2SuccessHandler.java
+++ src/main/java/com/example/productcrud/config/OAuth2SuccessHandler.java
@@ -3,6 +3,8 @@ package com.example.productcrud.config;
 import com.example.productcrud.model.CustomOAuth2User;
 import com.example.productcrud.model.CustomUserDetail;
 import com.example.productcrud.model.User;
+import com.example.productcrud.model.dto.reponse.AuthResponse;
+import com.example.productcrud.model.dto.reponse.UserResponse;
 import com.example.productcrud.service.impl.JwtService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -19,6 +21,9 @@ import java.io.IOException;
 public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
     private final JwtService jwtService;
 
+    @Value("${jwt.expiration}")
+    private String expiredIn;
+
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request,
                                         HttpServletResponse response,
@@ -27,15 +32,39 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
         CustomOAuth2User oAuth2User = (CustomOAuth2User) authentication.getPrincipal();
         User user = oAuth2User.getUser();
 
-
-        // generate token
         String token = jwtService.generateToken(new CustomUserDetail(user));
 
+        AuthResponse authResponse = AuthResponse.builder()
+                .accessToken(token)
+                .tokenType("Bearer")
+                .expiresIn(Long.valueOf(expiredIn))
+                .user(UserResponse.builder()
+                        .userId(user.getId())
+                        .userName(user.getUsername())
+                        .roles(user.getRoles())
+                        .build())
+                .build();
+
         response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
         response.getWriter().write("""
-            {
-              "token": "%s"
-            }
-        """.formatted(token));
+                {
+                  "accessToken": "%s",
+                  "tokenType": "%s",
+                  "expiresIn": %d,
+                  "user": {
+                    "userId": %d,
+                    "userName": "%s",
+                    "roles": %s
+                  }
+                }
+                """.formatted(
+                authResponse.getAccessToken(),
+                authResponse.getTokenType(),
+                authResponse.getExpiresIn(),
+                authResponse.getUser().getUserId(),
+                authResponse.getUser().getUserName(),
+                authResponse.getUser().getRoles().toString()
+        ));
     }
 }
